8f62c3de537c051af0e9ae5d7e02f9569afa1096
Andrew Smith <topcodeandrew@gmail.com>
2022-09-07 10:06:01 -0600

    data displayed on the FE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 const axios = require('axios')
 
-let handleClick = ()=>{
-  axios.post("http://localhost:5000/api/user_data", {"email": "john@gmail.com"}).then(res=>{console.log(res)})
-}
+
 
 function App() {
+  const [userInput, setUserInput] = useState("")
+  const [displayedUser, setDisplayedUser] = useState({fullName: "John Doe", gender: "Male", location: "Argentina"})
+
+
+  let handleClick = (info)=>{
+    let userInfo = {
+      "email": info
+    }
+
+    axios.post("http://localhost:5000/api/user_data", userInfo).then(res=>{
+      console.log(res.data)
+      setDisplayedUser(res.data)
+    })
+  }
+
+
   return (
     <div className="App">
-      <input placeholder='insert email or phone here'/>
-      <button onClick={handleClick}>BUTTON</button>
+      <input onChange={(e)=>{setUserInput(e.target.value)}} placeholder='insert email or phone here'/>
+      <button onClick={()=>{handleClick(userInput)}}>SEARCH EMAIL</button>
+      <h2>{displayedUser.fullName}</h2>
+      <h2>{displayedUser.gender ? displayedUser.gender : "No gender found"}</h2>
+      <h2>{displayedUser.location}</h2>
     </div>
   );
 }
