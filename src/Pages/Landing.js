61ff7cbe237b9edcad143aa90d0f8a2f863ad09e
Carlo <cearayon@gmail.com>
2022-09-12 15:02:51 -0700

    MVP completed

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,48 +1,70 @@
-import React, {useState} from 'react'
-import axios from 'axios'
-import {setNewQueriedUser} from '../redux/querySlice'
-import { useDispatch } from 'react-redux'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { setNewQueriedUser } from '../redux/querySlice';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-
 const Landing = () => {
-  const [userInput, setUserInput] = useState("")
+  const [userInput, setUserInput] = useState('');
+  const [dataPointType, setDataPointType] = useState('email');
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  let handleClick = (type, info) => {
+    let userInfo =
+      type === 'email'
+        ? {
+            email: info,
+          }
+        : {
+            phone: info,
+          };
 
-  let handleClick = (info)=>{
-    let userInfo = {
-      "email": info
-    }
-
-    axios.post("http://localhost:5000/api/user_data", userInfo).then(res=>{
-      console.log(res.data)
-      dispatch(setNewQueriedUser(res.data))
-      navigate('/results')
-    })
-  }
+    axios
+      .post('http://localhost:5000/api/user_data', userInfo)
+      .then((res) => {
+        console.log(res.data);
+        dispatch(setNewQueriedUser(res.data));
+        navigate('/results');
+      })
+      .catch((err) => alert('Could not find any associated data.'));
+  };
   return (
-    <div className="Landing">
+    <div className='Landing'>
       <div>
-        <h1> Your information is out there...</h1>
-        <h1> We can tell you how much</h1>
+        <h1 className='catchphrase'> Your information is out there...</h1>
+        <h1 className='catchphrase'> We can tell you how much</h1>
       </div>
       <div className='info-input'>
         <div className='first-line-info-input'>
           <h3> Find data related to the</h3>
-        <select style={{marginLeft:'10px'}}id="data-type">
-          <option value="email"> email </option>
-          <option value="phone"> phone number</option>
-        </select>
-          <h3 style={{margin:'0px 10px'}}>:</h3>
-          <input onChange={(e)=>{setUserInput(e.target.value)}} placeholder='insert email or phone here'/>
+          <select
+            onChange={(e) => setDataPointType(e.target.value)}
+            style={{ marginLeft: '10px' }}
+            id='data-type'
+          >
+            <option value='email'> email </option>
+            <option value='phone'> phone number</option>
+          </select>
+          <h3 style={{ margin: '0px 10px' }}>:</h3>
+          <input
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+            placeholder='insert email or phone here'
+          />
         </div>
-              <button onClick={()=>{handleClick(userInput)}}>SEARCH EMAIL</button>
+        <button
+          onClick={() => {
+            handleClick(dataPointType, userInput);
+          }}
+        >
+          SEARCH EMAIL
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
