b64f56af6f8a1863a2b14cef1c16cb8310966da8
Andrew Smith <topcodeandrew@gmail.com>
2022-09-12 10:05:40 -0600

    basic landing styling

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import axios from 'axios'
 import {setNewQueriedUser} from '../redux/querySlice'
 import { useDispatch } from 'react-redux'
-import Results from './Results'
 import { useNavigate } from 'react-router-dom';
 
 
@@ -25,10 +24,23 @@ const Landing = () => {
     })
   }
   return (
-    <div>
-      <input onChange={(e)=>{setUserInput(e.target.value)}} placeholder='insert email or phone here'/>
-    <button onClick={()=>{handleClick(userInput)}}>SEARCH EMAIL</button>
-    <Results/>
+    <div className="Landing">
+      <div>
+        <h1> Your information is out there...</h1>
+        <h1> We can tell you how much</h1>
+      </div>
+      <div className='info-input'>
+        <div className='first-line-info-input'>
+          <h3> Find data related to the</h3>
+        <select style={{marginLeft:'10px'}}id="data-type">
+          <option value="email"> email </option>
+          <option value="phone"> phone number</option>
+        </select>
+          <h3 style={{margin:'0px 10px'}}>:</h3>
+          <input onChange={(e)=>{setUserInput(e.target.value)}} placeholder='insert email or phone here'/>
+        </div>
+              <button onClick={()=>{handleClick(userInput)}}>SEARCH EMAIL</button>
+      </div>
     </div>
   )
 }
